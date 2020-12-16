f042dd50e77eeebffadb283b3509828d6d21c73c
gjonesdesign <mail@gjones.design>
2020-12-16 01:45:58 -0500

    remove dupe repo

---
+++ prototype_html/scripts/index.js
@@ -0,0 +1,17 @@
+
+function openNav() {
+	document.getElementById("mySidenav").style.width = "310px";
+	document.getElementById("nav").style.display = "block";
+	document.getElementById("btn").style.display = "none";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "55px";
+	document.getElementById("nav").style.display = "none"
+	3
+	document.getElementById("btn").style.display = "block";
+}
+
+function welcomeScroll() {
+	document.getElementById("scrollTo").scrollIntoView();
+}
