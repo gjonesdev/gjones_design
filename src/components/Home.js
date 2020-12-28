34efc99e9452463575f3af9f38ea40ae0f3e0778
gjonesdesign <mail@gjones.design>
2020-12-28 14:26:20 -0500

    started animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
 // cSpell:enableCompoundWords
 import React from "react";
 import { Link } from "react-router-dom";
+import Typist from "react-typist";
 
 function Home() {
 	return (
 		<div className="main-container">
 			<div className="main-text">
-				Hello There. <br /> My name is <u className="name-under"><u className="red-under">Gaetano</u> Jones</u> and I am a designer and developer.
-				<br />
+				<Typist>
+					Hello There.</Typist> <Typist>My name is <u className="name-under"><u className="red-under">Gaetano</u> Jones</u> and I am a designer and developer.
+				</Typist>
 				<br />
 				<Link to="/Work" className="link"><a>UI and Web Design</a></Link> are my primary focuses, <br /> but I like to make <Link to="/otherstuff" className="link">other stuff</Link> too.
 			</div>
