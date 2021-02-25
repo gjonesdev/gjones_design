29bac58d8d8044e977e2e25949cef347c289c12f
gjonesdesign <mail@gjones.design>
2021-02-25 00:33:12 -0500

    nav link layout, logo added, page link layout

--- src/components/OtherStuff.js
+++ src/components/OtherStuff.js
@@ -5,15 +5,29 @@ import Typist from "react-typist";
 
 function OtherStuff() {
 	return (
-		<div className="container">
-			<div className="main-text">
-				{/* <span className="block"></span> */}
+		<div>
+			{/* <div className="page-title">
 				<h1>Other Stuff.</h1><h2>Everyone has a junk drawer... <br />I just have some cool junk.</h2>
-				<Link to="/LogoDesign" className="link">Logo<br />Design</Link>
-				<Link to="/GraphicDesign" className="link">Graphic<br />Design</Link>
-				<Link to="/AugmentedReality" className="link">Augmented<br />Reality</Link>
-				<Link to="/CreativeCoding" className="link">Creative<br />Coding</Link>
 			</div>
+			<div>
+				<ul className="nav-list">
+					<li><Link to="/LogoDesign">Logo<br />Design</Link></li>
+					<li><Link to="/GraphicDesign">Graphic<br />Design</Link></li>
+					<li><Link to="/AugmentedReality">Augmented<br />Reality</Link></li>
+					<li><Link to="/CreativeCoding">Creative<br />Coding</Link></li>
+				</ul>
+			</div> */}
+			<div className="page-title">
+				<h1>Other Stuff.</h1><h2>Everyone has a junk drawer... <br />I just have some cool junk.</h2>
+			</div>
+			<div class="category-link">
+				<Link to="/LogoDesign">Logo<br />Design</Link>
+				<Link to="/GraphicDesign">Graphic<br />Design</Link>
+				<Link to="/AugmentedReality">Augmented<br />Reality</Link>
+				<Link to="/CreativeCoding">Creative<br />Coding</Link>
+			</div>
+			{/* <span className="block"></span> */}
+			{/* <span className="block"></span> */}
 		</div>
 	)
 }
--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -8,26 +8,27 @@ import "./Sidenav.css";
 function openNav() {
 	document.getElementById("navopenbtn").style.visibility = "hidden";
 	document.getElementById("navclosebtn").style.visibility = "visible";
-	document.getElementById("overlay").style.width = "16%";
+	document.getElementById("sidenav").style.width = "310px";
 	document.getElementById("dim").style.height = "100%";
-	document.getElementById("overlay-content").style.visibility = "visible";
+	document.getElementById("sidenav-content").style.visibility = "visible";
 };
 
 function closeNav() {
 	document.getElementById("navclosebtn").style.visibility = "hidden";
 	document.getElementById("navopenbtn").style.visibility = "visible";
-	document.getElementById("overlay-content").style.visibility = "hidden";
-	document.getElementById("overlay").style.width = "3%";
+	document.getElementById("sidenav-content").style.visibility = "hidden";
+	document.getElementById("sidenav").style.width = "55px";
 	document.getElementById("dim").style.height = "0%";
 };
 
 function Sidenav(props) {
 	return (
 		<div>
-			<div id="overlay">
+			<div id="sidenav">
 				<GoThreeBars id="navopenbtn" onClick={() => openNav()} />
 				<GoX id="navclosebtn" onClick={() => closeNav()} />
-				<div id="overlay-content">
+				<div id="sidenav-content">
+					<div className="logo"><u className="red-under">Gaetano</u><br />Jones</div>
 					<Link to="/Home" onClick={() => closeNav()} >Home</Link>
 					<Link to="/About" onClick={() => closeNav()} >About Me</Link>
 					<Link to="/Work" onClick={() => closeNav()} >My Work</Link>
