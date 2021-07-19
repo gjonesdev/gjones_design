e6e5562847393ae40d0397d94c20714906bc2df5
gjonesdesign <mail@gjones.design>
2021-07-19 16:22:32 -0400

    finished about page, popup. gjonesedesign v.99 deploy

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?
-$to_email = "mail@gjones.design";
+$mail_to_send_to = "mail@gjones.design";
 $from_email = "inquiry@gjones.design";
 $sendflag = $_REQUEST['sendflag'];    
 $name=$_REQUEST['name'];
@@ -10,7 +10,7 @@ if ( $sendflag == "send" )
                 $message= "\r\n" . "Name: $name" . "\r\n"; //get recipient name in contact form
                 $message = $message.$_REQUEST['message'] . "\r\n" ;//add message from the contact form to existing message(name of the client)
                 $headers = "From: $from_email" . "\r\n" . "Reply-To: $email"  ;
-                $a = mail($to_email, $subject, $message, $headers);
+                $a = mail($mail_to_send_to, $subject, $message, $headers);
                 if ($a)
                 {
                      print("Message was sent, you can send another one");
