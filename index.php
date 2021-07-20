c9aa200f81e105fabfa6787ff9a333e598684850
gjonesdesign <mail@gjones.design>
2021-07-20 17:01:41 -0400

    added smaller thumbnail files, optimized images, created initial mobile layout

--- index.php
+++ index.php
@@ -7,15 +7,15 @@ if ( $sendflag == "send" )
         {
                 $subject= "Message subject";
                 $email = $_REQUEST['email'] ;
-                $message= "\r\n" . "Name: $name" . "\r\n"; //get recipient name in contact form
+				$message= "\r\n" . "Name: $name" . "\r\n" ."Email: $email" . "\r\n"; //get recipient name in contact form
                 $message = $message.$_REQUEST['message'] . "\r\n" ;//add message from the contact form to existing message(name of the client)
                 $headers = "From: $from_email" . "\r\n" . "Reply-To: $email"  ;
                 $a = mail($mail_to_send_to, $subject, $message, $headers);
                 if ($a)
                 {
-                     print("Message was sent, you can send another one");
+                     print("Message received. I apologize for this rudimentary confirmation page while I work on a more appealing message. Speak to you soon!");
                 } else {
-                     print("Message wasn't sent, please check that you have changed emails in the bottom");
+                     print("There has been an error sending your message, please make sure your email is valid. I apologize for this rudimentary page while I work on a more appealing message.");
                 }
         }
 		?>
