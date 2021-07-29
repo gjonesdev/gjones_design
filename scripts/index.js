4d9e89ac01fa4c7f2c821ada62e43a099224479e
gjonesdesign <mail@gjones.design>
2021-07-29 16:34:09 -0400

    testing callback

--- scripts/index.js
+++ scripts/index.js
@@ -549,7 +549,7 @@ function bootscreen() {
 }
 
 async function leaderscreen(new_user, new_score) {
-	var leader_array = await fetch_leader('leaderboard.json');
+	// var leader_array = await fetch_leader('leaderboard.json');
 	if (new_score != null) {
 		console.log(new_score, new_user);
 		fetch('leaderboard.php', {
