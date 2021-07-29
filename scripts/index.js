988a2a7882e3c16f4731f343d24363524bf33ad9
gjonesdesign <mail@gjones.design>
2021-07-29 16:53:01 -0400

    testing callback

--- scripts/index.js
+++ scripts/index.js
@@ -586,7 +586,7 @@ async function leaderscreen(new_user, new_score) {
 function gameoverscreen() {
 	game_run = false;
 	document.getElementById("bootscreen").innerHTML = "<h1>Game Over. Press Enter to play Again.</h1> <h1>Your Final Score:</h1>" +
-		"<form onsubmit='leaderscreen(this.user.value, this.score.value, e);return false'> <input id='score' type='text' name='score' value='" + points + "' readonly> <input type='text' name='user' placeholder='Your Username' maxlength='5'> <input class='submit-button' type='submit' value='Submit'> </form>";
+		"<form onsubmit='leaderscreen(this.user.value, this.score.value);return false;'> <input id='score' type='text' name='score' value='" + points + "' readonly> <input type='text' name='user' placeholder='Your Username' maxlength='5'> <input class='submit-button' type='submit' value='Submit'> </form>";
 	document.getElementById("bootscreen").style.display = "block";
 	document.getElementById("bootscreen").style.gridRow = "2/4";
 	document.getElementById("bootscreen").style.alignSelf = "center";
