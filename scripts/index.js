300503dcbd84f9f743572dbc4b2a6f063fd00ae2
gjonesdesign <mail@gjones.design>
2021-07-29 14:20:27 -0400

    testing callback

--- scripts/index.js
+++ scripts/index.js
@@ -554,7 +554,7 @@ function bootscreen() {
 }
 
 function create_leader(scores) {
-	// console.log(scores.data[0]);
+	console.log(scores[0]);
 	// scores.sort(function (a, b) {
 	// 	return b.score - a.score;
 	// });
