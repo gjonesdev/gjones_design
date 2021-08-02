bc10ef71d185ec22d4b73ecd736db18c6f46b328
gjonesdesign <mail@gjones.design>
2021-08-02 16:04:57 -0400

    leaderboard 1.0, contact form update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $mail_to_send_to = "mail@gjones.design";
 $from_email = "inquiry@gjones.design";   
 $name=$_REQUEST['name'];
 
-$subject= "$subject";
+$subject= $_REQUEST['$subject'];
 $email = $_REQUEST['email'] ;
 $message= "\r\n" . "Name: $name" . "\r\n" ."Email: $email" . "\r\n"; //get recipient name in contact form
 $message = $message.$_REQUEST['message'] . "\r\n" ;//add message from the contact form to existing message(name of the client)
